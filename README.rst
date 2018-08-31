e64cc6195cdd0770e202c0f3cf683f984f28673c
Matt Magin <matt.magin@cmv.com.au>
2018-08-31 15:03:30 +0930

    Added some logging and updated documentation to be accurate

--- README.rst
+++ README.rst
@@ -16,8 +16,16 @@ Usage
 
     # Import
     from decouple import Config
-    from decouple_aws import RepositoryAwsSecretManager
+    from decouple_aws import get_config, RepositoryAwsSecretManager
 
+    # The package provides a wrapper function that will
+    # fallback to environment variables and fail gracefully
+    # if AWS Secrets Manager is not accessible for whatever
+    # reason.
+    config = get_config('your/secret/name', 'ap-southeast-2')
+
+    # Alternatively, if you would like it to fail if secrets
+    # manager is inaccessible, you can build it manually.
     # initialise the config with the AWS repository
     # Pass the repo your secret name and the region
     repo = RepositoryAwsSecretManager('your/secret/name', 'ap-southeast-2')
--- decouple_aws/__init__.py
+++ decouple_aws/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 from decouple import AutoConfig, Config
 from botocore.exceptions import BotoCoreError
 
@@ -6,11 +8,19 @@ from .repository import RepositoryAwsSecretManager
 # We do this assertion so pyflakes doesn't complain
 assert RepositoryAwsSecretManager
 
+logger = logging.getLogger(__name__)
+
 
 def get_config(source, region):
     """ Get config object but fallback to AutoConfig if AWS connection fails """
     try:
+        logger.debug(
+            'Querying AWS Secrets manager for %s in region %s', source, region)
         repo = RepositoryAwsSecretManager(source, region)
+        logger.debug('Successfully queried for %s in region %s',
+                     source, region)
         return Config(repo)
-    except BotoCoreError:
+    except BotoCoreError as e:
+        logger.error(
+            'Failed retrieving secrets from AWS Secrets Manager: %s', e)
         return AutoConfig()
